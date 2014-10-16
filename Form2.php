e89024d437af01706a4b0ceba054074c9ef3e0fc
stmacebal <mateo.cebquerol@gmail.com>
2014-10-16 16:51:14 +0200

    Add Form2.php

---
+++ Form2.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<?php
+// define variables and set to empty values
+$name =1;
+$email =2;
+$gender =3;
+$comment =4; 
+$website =5;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+</body>
+</html>
